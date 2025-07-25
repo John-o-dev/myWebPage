ab224df6e7a426934f3ac07da62311c434aa2ea8
John-o-dev <johnny.o.developer@gmail.com>
2025-07-25 14:51:48 -0300

    Fix bugs in icons, changing for svg icons

--- src/components/Sidebar/SidebarInfoMore/index.tsx
+++ src/components/Sidebar/SidebarInfoMore/index.tsx
@@ -15,7 +15,10 @@ export default function SidebarInfoMore() {
 
             <button className={styles.info_more_btn} onClick={toggleMenu}>
                 <span>{t(className, 'contacts')}</span>
-                <span className="material-symbols-outlined">arrow_downward</span>
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+                </svg>
             </button>
 
             <ul className={`${styles.contacts_list} ${menuOpen ? styles.active : ""}`}>
