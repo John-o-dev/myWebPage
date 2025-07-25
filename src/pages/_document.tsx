744e56d61bfd6a525678cb114e0befb18aacdefe
John-o-dev <johnny.o.developer@gmail.com>
2025-07-25 14:20:23 -0300

    Fix new bug in google materials icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,7 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap" />
           <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
         </Head>
         <body>
