395fe2a0798a6017caa8f072fcdfa11456359b5f
John-o-dev <johnny.o.developer@gmail.com>
2025-07-26 17:04:14 -0300

    Updating icons

--- src/components/Sidebar/SidebarInfoMore/index.tsx
+++ src/components/Sidebar/SidebarInfoMore/index.tsx
@@ -16,10 +16,6 @@ export default function SidebarInfoMore() {
             <button className={styles.info_more_btn} onClick={toggleMenu}>
                 <span>{t(className, 'contacts')}</span>
                 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
-                </svg>
-
                 <span className="material-symbols-outlined">arrow_downward</span>
             </button>
 
